5e58186d5bc8de656e41ec82183174c84af6cd90
Dios Malone <dios.malone@gmail.com>
2018-03-24 09:46:41 +0800

    Implemente data generator

--- model.py
+++ model.py
@@ -8,25 +8,39 @@ with open('data/driving_log.csv') as csvfile:
    reader = csv.reader(csvfile)
    for line in reader:
       lines.append(line)
-      
-images = []
-measurements = []
 lines = lines[1:] #skip the first headline
-for line in lines:
-   source_path = line[0]
-   filename = source_path.split('/')[-1]
-   current_path = 'data/IMG/' + filename
-   image = cv2.imread(current_path)
-   b,g,r = cv2.split(image)
-   image = cv2.merge([r,g,b])
-   images.append(image)
-   measurement = float(line[3])
-   measurements.append(measurement)
-   
-X_train = np.array(images)
-y_train = np.array(measurements)
 
+from sklearn.model_selection import train_test_split
+train_samples, validation_samples = train_test_split(lines, test_size=0.2)
 
+def generator(samples, batch_size=32):
+   num_samples = len(samples)
+   while 1: # Loop forever so the generator never terminates
+      shuffle(samples)
+      for offset in range(0, num_samples, batch_size):
+         batch_samples = samples[offset:offset+batch_size]
+         images = []
+         measurements = []
+         for batch_sample in batch_samples:
+            source_path = line[0]
+            filename = source_path.split('/')[-1]
+            current_path = 'data/IMG/' + filename
+            image = cv2.imread(current_path)
+            b,g,r = cv2.split(image)
+            image = cv2.merge([r,g,b])
+            images.append(image)
+            measurement = float(line[3])
+            measurements.append(measurement)
+
+         # trim image to only see section with road
+         X_train = np.array(images)
+         y_train = np.array(measurements)
+         yield sklearn.utils.shuffle(X_train, y_train)
+
+# compile and train the model using the generator function
+train_generator = generator(train_samples, batch_size=32)
+validation_generator = generator(validation_samples, batch_size=32)            
+            
 #Model Architecture
 from keras.models import Sequential
 from keras.layers import Flatten, Dense, Lambda
@@ -52,5 +66,7 @@ model.add(Dense(1))
 
 model.compile(loss='mse', optimizer='adam')
 model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=2)
+model.fit_generator(train_generator, sample_per_epoch=len(train_samples), \
+                     validation_data = validation_generator, nb_val_samples=len(validation_samples), epochs=2)
 
 model.save('model.h5')
